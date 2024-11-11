25d15c4fa5d906e5e12c15f48241b1649dbd6d54
lov-etan <emily1290@naver.com>
2024-11-12 04:40:27 +0900

    feat: Update calendar page view layout
    - Modified view layout on the calendar page
    - Further color adjustments needed for final look

--- eatory-vueproject/src/main.js
+++ eatory-vueproject/src/main.js
@@ -1,4 +1,4 @@
-//import './assets/main.css' // 기존 main.css 파일
+import './assets/main.css' // 기존 main.css 파일
 //import './assets/base.css' // base.css 파일이 생성된 경우에만 추가
 
 import { createApp } from 'vue'
@@ -6,10 +6,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import VCalendar from 'v-calendar'
+
+// v-calendar 기본 스타일 가져오기
+import 'v-calendar/style.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VCalendar, {})
 
 app.mount('#app')
